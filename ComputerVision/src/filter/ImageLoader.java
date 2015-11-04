ebbd6f2cdf02ac1bd1374c777330392ef80af6e4
Pavlo <paolokoelio@gmail.com>
2015-11-04 11:17:20 +0100

    caricamento immagine

--- ComputerVision/src/filter/ImageLoader.java
+++ ComputerVision/src/filter/ImageLoader.java
@@ -14,27 +14,27 @@ public class ImageLoader {
 		super();
 		this.filename = filename;
 	}
-	
-	public int[][] imageToMatrix() throws IOException{
-		
+
+	public int[][] imageToMatrix() throws IOException {
+
 		BufferedImage bufferedImage = ImageIO.read(new File(filename));
-		
+
 		int height = bufferedImage.getHeight();
 		int width = bufferedImage.getWidth();
-				
-				
+
+		System.out.println(height + " " + width);
+
 		int[][] matrix = new int[height][width];
-		
-		
-		
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				matrix[i][j] = bufferedImage.getRGB(i, j);
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.println(j + " " + i);
+				matrix[i][j] = bufferedImage.getRGB(j, i);
 			}
-		}		
-		
+		}
+
 		return matrix;
-		
+
 	}
-	
+
 }
