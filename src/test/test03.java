1740a7e0d3f47c6067d9443aed7573d350ce51f4
Pavlo <paolokoelio@gmail.com>
2015-11-05 17:15:27 +0100

    prova2

--- src/test/test03.java
+++ src/test/test03.java
@@ -10,31 +10,25 @@ import noise.SaltAndPepper;
 public class test03 {
 
 	public static void main(String[] args) {
-		
-		
-		float up = (float)0.9;
-		float down = (float)0.1;
-		
+
+		float up = (float) 0.9;
+		float down = (float) 0.1;
+
 		ImageLoader image = new ImageLoader("image/inverno.jpg");
 
 		INoise noise = new SaltAndPepper(up, down);
-		
-		
+
 		try {
 			int[][] matrix = image.imageToMatrix();
 			int[][] noisedMatrix = new int[matrix.length][matrix[0].length];
 			noisedMatrix = noise.addNoise(matrix);
-			
+
 			Utils.printImage(Utils.matrixToBuffered(noisedMatrix), "prova");
-			
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
 	}
 }
