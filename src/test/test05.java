031667348eddd80f162505db99c4fa66d06eee37
Pavlo <paolokoelio@gmail.com>
2015-11-09 14:55:20 +0100

    Filtro guassiano completo e funzionante per kernel di diverse dimensioni.

--- src/test/test05.java
+++ src/test/test05.java
@@ -12,25 +12,30 @@ public class test05 {
 	public static void main(String[] args) {
 
 		ImageLoader image = new ImageLoader("image/inverno.jpg");
-		
-		int DIM = 3;
-		
+
+		int DIM = 7; //works only with 3 because of the filter
+		double sigma1 = 9;
+		double sigma2 = 0.5;
+
 		try {
 			int[][] matrix = image.imageToMatrix();
+
+			DOGFilter dogFilter = new DOGFilter(matrix,DIM);
+
+			System.out.print("First Gaussian with sigma " + sigma1);
+			GaussianMatrix gauss1 = new GaussianMatrix(sigma1, DIM);
 			
-			DOGFilter dogFilter = new DOGFilter(matrix);
-			
-			GaussianMatrix gauss1 = new GaussianMatrix(0.1, DIM);
-			GaussianMatrix gauss2 = new GaussianMatrix(1, DIM);
-			
+			System.out.print("Second Gaussian with sigma " + sigma2);
+			GaussianMatrix gauss2 = new GaussianMatrix(sigma2, DIM);		
+
 			dogFilter.setGauss(gauss1.getMatrix(), gauss2.getMatrix());
-			Utils.printImage(Utils.matrixToBuffered(dogFilter.getDogMatrix(DIM)), "dog " + DIM);
-			
+			Utils.printImage(Utils.matrixToBuffered(dogFilter.getDogMatrix()), "dog " + DIM +"x" + DIM);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
