1a3cbc7d575d5448dde53d8931f206f813ea1239
alberto <alberto.digioacchino01@universitadipavia.it>
2015-11-11 09:16:38 +0100

    Added Nagao

--- ComputerVision/src/cleanNoise/Nagao.java
+++ ComputerVision/src/cleanNoise/Nagao.java
@@ -1,13 +1,18 @@
 package cleanNoise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Nagao {
 
 	private int[][] matrix;
 	private List<List<Integer>> list = new ArrayList<List<Integer>>(9);
+	private List<Float> meanList = new ArrayList<Float>(9);
 
+	
 	public Nagao(int[][] matrix) {
 		super();
 		this.matrix = matrix;
@@ -15,12 +20,13 @@ public class Nagao {
 
 	public void fillLists(int indexI, int indexJ)
 			throws NoMatrixElementException {
+		list.clear();
 		if (indexI < 2 || indexJ < 2 || indexI > matrix.length - 3
-				|| indexJ > matrix.length - 3)
+				|| indexJ > matrix[0].length - 3)
 			throw new NoMatrixElementException("NoElement");
 
 		List<Integer> central = new ArrayList<Integer>(9);
-		System.out.println("ahfhafh" + indexI + " " + indexJ);
+	//	System.out.println("ahfhafh" + indexI + " " + indexJ);
 		for (int i = indexI - 1; i < indexI + 2; i++) {
 			for (int j = indexJ - 1; j < indexJ + 2; j++) {
 				central.add(matrix[i][j]);
@@ -37,7 +43,7 @@ public class Nagao {
 		}
 
 		List<Integer> bottom = new ArrayList<Integer>(7);
-		top.add(matrix[indexI][indexJ]);
+		bottom.add(matrix[indexI][indexJ]);
 		for (int i = indexI + 1; i < indexI + 3; i++) {
 			for (int j = indexJ - 1; j < indexJ + 2; j++) {
 				bottom.add(matrix[i][j]);
@@ -45,14 +51,14 @@ public class Nagao {
 		}
 
 		List<Integer> left = new ArrayList<Integer>(7);
-		top.add(matrix[indexI][indexJ]);
+		left.add(matrix[indexI][indexJ]);
 		for (int i = indexI - 1; i < indexI + 2; i++) {
 			for (int j = indexJ - 2; j < indexJ; j++) {
 				left.add(matrix[i][j]);
 			}
 		}
 		List<Integer> right = new ArrayList<Integer>(7);
-		top.add(matrix[indexI][indexJ]);
+		right.add(matrix[indexI][indexJ]);
 		for (int i = indexI - 1; i < indexI + 2; i++) {
 			for (int j = indexJ + 1; j < indexJ + 3; j++) {
 				right.add(matrix[i][j]);
@@ -119,5 +125,57 @@ public class Nagao {
 
 	public List<List<Integer>> getList() {
 		return list;
+	}
+	
+	public void computeMeans() {
+		meanList.clear();
+		for (List<Integer> list2 : list) {
+			int sum = 0;
+			int counter = 0;
+			for (Integer integer : list2) {
+				sum += integer;
+				counter++;
+			}
+			meanList.add((float)sum/counter);
+		}
+	}
+	
+	public float getVariance(int index) {
+		List<Integer> l = list.get(index);
+		float mean = meanList.get(index);
+		float sum = 0;
+		for (Integer integer : l) {
+			sum += (integer-mean)*(integer-mean);
+		}
+		//System.out.println("VARiance = " + (float)sum/l.size()+ " size = " + l.size());
+		return (float)sum/l.size();
+	}
+	
+	public int[][] filter() {
+		int[][] filtered = new int[matrix.length][matrix[0].length];
+		System.out.println(matrix.length+" "+ matrix[0].length);
+		for (int i = 0; i < filtered.length; i++) {
+			for (int j = 0; j < filtered[0].length; j++) {
+				filtered[i][j] = matrix[i][j];
+			}
+		}
+		
+		for (int i = 0; i < filtered.length; i++) {
+			for (int j = 0; j < filtered[0].length; j++) {
+				try {
+					fillLists(i, j);
+					computeMeans();
+					LinkedList<Float> f = new LinkedList<Float>();
+					for (int j2 = 0; j2 < list.size(); j2++) {
+						f.add(getVariance(j2));
+					}
+					float min = Collections.min(f);
+					int indexMin = f.indexOf(min);
+					filtered[i][j] = meanList.get(indexMin).intValue();
+				} catch (NoMatrixElementException e) {
+				}
+			}
+		}
+		return filtered;
 	}
 }
--- ComputerVision/src/test/test03.java
+++ ComputerVision/src/test/test03.java
@@ -2,6 +2,7 @@ package test;
 
 import java.io.IOException;
 
+import cleanNoise.Nagao;
 import filter.ImageLoader;
 import filter.Utils;
 import noise.INoise;
@@ -12,8 +13,8 @@ public class test03 {
 	public static void main(String[] args) {
 		
 		
-		float up = (float)0.9;
-		float down = (float)0.1;
+		float up = (float)0.99;
+		float down = (float)0.01;
 		
 		ImageLoader image = new ImageLoader("image/inverno.jpg");
 
@@ -24,8 +25,11 @@ public class test03 {
 			int[][] matrix = image.imageToMatrix();
 			int[][] noisedMatrix = new int[matrix.length][matrix[0].length];
 			noisedMatrix = noise.addNoise(matrix);
-			
-			Utils.printImage(Utils.matrixToBuffered(noisedMatrix), "prova");
+			Nagao nag = new Nagao(noisedMatrix);
+			Utils.printImage(Utils.matrixToBuffered(nag.filter()), "pulwwite");
+			Utils.printImage(Utils.matrixToBuffered(noisedMatrix), "sporca");
+			Utils.printImage(Utils.matrixToBuffered(matrix), "prova");
+
 			
 			
 		} catch (IOException e) {
